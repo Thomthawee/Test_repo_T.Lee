92ccb7ad08bf71249807a90f6a49d75e911cd6a1
Thomthawee <thomthawee@uchicago.edu>
2022-03-31 16:23:06 -0500

    Lecture 1
    Just Note

---
+++ Lecture Note.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:59:49 2022
+
+@author: thomthawee
+"""
+#Class 1
+#Just Note how to use repo
+
+#1. create a repo (via Github desktop)
+#2. Clone locally
+#3. .py and save in the cloned folder
+#4. commit/push
+#5. Share with member (only in website)
