f42388185a51f9423d97ecd3f54e4e6cd5d2a489
Thomthawee <thomthawee@uchicago.edu>
2022-04-09 11:51:50 -0500

    Week 2
    Week 2 : finish

--- Lecture Note.py
+++ Lecture Note.py
@@ -36,7 +36,7 @@ First import datetime <- python does not have basic datatype that can be used wi
     
 -Boolean : T and F *** need '==' to prove T or F
 
--string : world that actually assign with '',"" and can use + operator to merge
+-string : word that actually assign with '',"" and can use + operator to merge
 *** string.method/attribute()
 *** String Formatting
 use .string(var_name) <- look at lecture (line 67-75)
@@ -57,7 +57,7 @@ use .string(var_name) <- look at lecture (line 67-75)
 
 **Terminology***
 
-class  = blue-print (instruct you what you should have)
+class  = blue-print (instruct you what you should have) , All data types = class
 instance = object e.g. garage
 *We can create many houses from 1 blue print = we can create many instance from 1 class*
 
@@ -73,4 +73,51 @@ x =+ number <- it help to add the value from existing variable (x)
 .capitalize() <- Capital Letter
 .strip() <- Find the first letter to be the first word                                             
 
+***Function***
+set() = return unique value
+list() = change any type to list
+"""
+#Week 2 , Class 1 : loop and logic
+"""
+-Boolean : More complex
+    - in , not in <- check 'things' in or not in list
+-Boolean take us to use if statement : T do sth. not do sth.
+
+- If : If ,elif==else if , else
+    - Should have : after first if elif else line
+
+- For loop:
+    - For 'suitable name of element' in name of list:
+    - !!! must choose good name (i is suitable for 0-n)
+
+- Break and Continue
+    -Break : Force to stop loop when faced true
+    -Continue : Skip when condition is True
+
+- While : Keep iterating until it give back False
+
+-list comprehension : iteration that start and end with list
+    - kind of for loop if else in 1 line that return in list
+    - vari = [function of value (f(v)) for value in start_list] : mapping
+    - vari = [function of value (f(v)) for value in start_list] : mapping and filtering
+    - Trick: start reading with for , if and lastly is method(function)
+
+- Iterating over dictionaries : need to use () (method) to run the loop
+    - dic = {Key:values}
+    - for keys in dic.keys() : use Key
+    -for value in dic.values() : use values
+    -for key,value in dic.items() : use both
+        -Interlude : Unpacking Notation . So, we can use key,value
+    - Can use dictionary comprehension : {fn of keys, fn of values for keys,values in dic.items()}
+    
+
+- Intermediate output : use to check something in the loop (especially print(element))
+    
+***Method***
+- .startwith()
+- .isnumeric()
+
+***Function***
+range(number) = [0,number-1] **generate from zero
+#List of List
 """
